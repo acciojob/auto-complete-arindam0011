0477a993bb4d93ad93d316f7c0ac14f1770a31aa
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-08-28 18:15:56 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
 import React from "react";
 import './../styles/App.css';
+import {useState} from "react";
 
 const App = () => {
+  const[ filterInput, setFilterInput ] = useState("")
+
+ const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="main">
+      <h1 id="heading">Search Item</h1>
+       <input id="input" type="text" value={filterInput} onChange={(e) => setFilterInput(e.target.value)}/>
+       <ul id="list">
+        {
+          fruits.filter((fruit) => fruit.toLowerCase().includes(filterInput.toLowerCase())).map((fruit) => <li>{fruit}</li>)
+        }
+       </ul>
     </div>
   )
 }
